6dc2fac4965569b93c99d296b26cf64577231006
Salah Ezzat <salahezzat059@gmail.com>
2025-01-25 22:39:42 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // redirectTo can ba a function that returns a string
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent ,title: 'Home'},
+  {path: 'about' , component: AboutComponent ,title: 'About'},
+  {path: 'portfolio', component: PortfolioComponent ,title: 'Portfolio'},
+  {path: 'contact' , component: ContactComponent, title: 'Contact'},
+  {path: '**', component: NotfoundComponent, title: 'Not Found'}
+];
